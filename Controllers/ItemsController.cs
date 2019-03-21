62daf1f788671f1427edddbb39b4a79fe3840fb6
Fortino Sandoval <fortino.sandoval@maritz.com>
2019-03-21 14:08:03 -0700

    return created response

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -29,7 +29,7 @@ namespace api.Controllers
     {
       _Context.Add(item);
       await _Context.SaveChangesAsync();
-      return Ok(item);
+      return Created("api/Items", item);
     }
 
     [HttpDelete("{id}")]
