693fd29e3bcbc6345e93fc08853e44d7cfc824ee
Angel Daniel Munoz Gonzalez <scythes.lol@hotmail.com>
2019-03-22 14:25:50 -0600

    rename item model for items

---
+++ MarketOrganizer.Data/Migrations/20190322201527_RenameItemTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MarketOrganizer.Data.Migrations
+{
+    public partial class RenameItemTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ItemName = table.Column<string>(nullable: true),
+                    BuyPrice = table.Column<int>(nullable: false),
+                    SellPrice = table.Column<int>(nullable: false),
+                    HighestMarketPrice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BuyPrice = table.Column<int>(nullable: false),
+                    HighestMarketPrice = table.Column<int>(nullable: false),
+                    ItemName = table.Column<string>(nullable: true),
+                    SellPrice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                });
+        }
+    }
+}
