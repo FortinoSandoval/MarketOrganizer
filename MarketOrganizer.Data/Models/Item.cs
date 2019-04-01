f1ff112bedb14798a9b06142d756ba9adce27fab
Fortino Sandoval <fortino.sandoval@maritz.com>
2019-04-01 11:24:39 -0700

    update decimal prices for integer prices

--- MarketOrganizer.Data/Models/Item.cs
+++ MarketOrganizer.Data/Models/Item.cs
@@ -12,8 +12,8 @@ namespace MarketOrganizer.Data.Models
   {
     public int Id { get; set; }
     public string ItemName { get; set; }
-    public decimal BuyPrice { get; set; }
-    public decimal SellPrice { get; set; }
-    public decimal HighestMarketPrice { get; set; }
+    public int BuyPrice { get; set; }
+    public int SellPrice { get; set; }
+    public int HighestMarketPrice { get; set; }
   }
 }
